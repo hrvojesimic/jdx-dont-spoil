2f5de7a330b4e19fc876b4a4b7cc2d765ee90293
hsimic <hrvojesimic@gmail.com>
2022-07-01 11:56:23 +0200

    kreiranje Spring Boot projekta

---
+++ src/test/java/jdx/dontspoil/DontspoilApplicationTests.java
@@ -0,0 +1,13 @@
+package jdx.dontspoil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontspoilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
